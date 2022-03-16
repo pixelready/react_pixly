516b63d907b6276398f0d10f8df8cf5076c8ab23
pixelready <bizartist@gmail.com>
2022-03-16 10:22:34 -0700

    WIP s3 upload

--- pixly-backend-express/api.js
+++ pixly-backend-express/api.js
@@ -1,11 +1,46 @@
+// Load the AWS SDK for Node.js
+import AWS from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
 import EXIF from "exif-js";
+
+
+// Set the AWS region 
+AWS.config.update({region: 'REGION'});
 
 const SAVE_IMAGE_PATH = "../images/";
 
 class imageFileHandler{
-    static async saveToStorage(){
-        //TODO: write file to public images folder
-        //TODO: update to point to S3
+    static async saveToStorage(file){
+        
+        // Create S3 service object
+        const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+        // call S3 to retrieve upload file to specified bucket
+        const uploadParams = {Bucket: BUCKET_NAME, Key: '', Body: ''};
+
+        // Configure the file stream and obtain the upload parameters
+        
+        const fileStream = fs.createReadStream(file);
+            fileStream.on('error', function(err) {
+            console.log('File Error', err);
+        });
+
+        uploadParams.Body = fileStream;
+        uploadParams.Key = path.basename(file);
+
+        // call S3 to retrieve upload file to specified bucket
+        s3.upload (uploadParams, function (err, data) {
+            if (err) {
+                console.log("Error", err);
+                return err;
+            } if (data) {
+                console.log("Upload Success", data.Location);
+                return data.location;
+            }
+         });
+         
+         //TODO: handle return values from s3 upload
     }
 
     static extractExif(){
--- plan.md
+++ plan.md
@@ -40,15 +40,28 @@
 
 Upload w/ Title -> Static Folder + JSON Server Metadata (/Images)
 
+- Sample EXIF images: https://github.com/ianare/exif-samples/tree/master/jpg
+
 - ROUTES (JSON Server):
 
   - Done: json server setup
   - POST /images
-  - //TODO: Create Model methods for images save and get (save to folder)
+  - //TODO: Create Model methods for images save and get (save to s3)
 
 - Extract & parse EXIF Data
+
+  - Fields:
+
+    - Make: "Apple" <string>
+    - Model: "iPhone 11" <string>
+    - ISOSpeedRatings: 800 <integer>
+    - FocalLengthIn35mmFilm: 30 <integer>
+    - DateTime: "2022:03:07 19:00:21" <string/DateTime>
+    - PixelXDimension: 2316 <integer>
+    - PixelYDimension: 3088 <integer>
+
   - //Done: find EXIF library
-  - //TODO: handle file upload
+  - //TODO: handle file upload to S3
 
 View -> Uploaded files (React) \* GET /images
 
